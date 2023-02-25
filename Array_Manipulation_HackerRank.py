d2cd7acf7ff7333d5e44ac118d95565f58996f05
Ekram Kedir <102755583+ekramkedir2020@users.noreply.github.com>
2023-02-26 01:13:48 +0300

    Create Array_Manipulation_HackerRank.py

---
+++ Array_Manipulation_HackerRank.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'arrayManipulation' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+def arrayManipulation(n, queries):
+    # Write your code here
+    answer = [0] * (n + 2)
+    for row,col,val in queries:
+       answer[row] += val
+       answer[col + 1] -= val
+   
+    for ans in range(1,len(answer)):
+        answer[ans] += answer[ans - 1]
+    return max(answer)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
